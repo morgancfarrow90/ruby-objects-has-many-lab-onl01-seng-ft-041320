d3767ea07173eea16f030891853fdbfda98f7b36
Morgan Farrow <morgancfarrow@gmail.com>
2020-04-28 22:24:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,21 +16,6 @@ class Artist
     add_song(song)
   end
   
-  def artist_name=(name)
-    if (self.artist.nil?)
-      self.artist = Artist.new(name)
-    else
-      self.artist.name = name
-    end
-  end
-
-  
-  
-  
-  
-  
-  
- 
   def songs
     Song.all.select {|song| song.artist == self}
   end
